cf17d5f08161e0ae4375fea3d14e11170b70dfe6
TopCuteGamer <64685151+TopCuteGamer@users.noreply.github.com>
2020-07-09 13:18:10 +0900

    Reset

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,12 @@
-import discord
 from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@client.event
-async def on_ready():
-    game = discord.Game(".help | Artemis.exe")
-    await client.change_presence(status=discord.Status.idle, activity=game)
-
-@client.event
+@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
